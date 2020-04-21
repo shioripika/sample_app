51f4479ed337d92ec250265070ad7739be13758a
Shiori <pikashu.get.daze.ww@gmail.com>
2020-04-21 08:02:39 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  
   root 'static_pages2#home'
   
-  get 'static_pages2/home'
-  get 'static_pages2/help'
-  get 'static_pages2/about'
-  get 'static_pages2/contact'
- 
+  get '/help', to: 'static_pages2#help'
+  get '/about', to: 'static_pages2#about'
+  get '/contact', to: 'static_pages2#contact'
+  
 end
