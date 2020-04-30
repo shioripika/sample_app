c8a596910158219c0bf26cd092872c9e23ff8e6f
Shiori <pikashu.get.daze.ww@gmail.com>
2020-04-30 08:40:38 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  
   root 'static_pages2#home'
-  
   get '/help', to: 'static_pages2#help'
   get '/about', to: 'static_pages2#about'
   get '/contact', to: 'static_pages2#contact'
-  
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
