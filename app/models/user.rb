3d6d61892bae93e7e47f0838e79e33e4e0cff1ab
Shiori <pikashu.get.daze.ww@gmail.com>
2020-05-24 15:31:23 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   #remember_token属性をUserクラスに定義
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
@@ -67,6 +68,12 @@ class User < ApplicationRecord
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,10 +13,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
    assert_select "a[href=?]", contact_path
    assert_select "a[href=?]", signup_path
    assert_select "a[href=?]", login_path
-   assert_select "a[href=?]", users_path, count: 0
-   assert_select "a[href=?]", user_path(@user), count: 0
-   assert_select "a[href=?]", edit_user_path(@user),count: 0
-   assert_select "a[href=?]", logout_path, count: 0
    get contact_path
    assert_select "title", full_title("Contact")
    get signup_path
@@ -36,8 +32,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
    assert_select "a[href=?]", help_path
    assert_select "a[href=?]", about_path
    assert_select "a[href=?]", contact_path
-   assert_select "a[href=?]", login_path, count: 0
-   assert_select "a[href=?]", signup_path
    assert_select "a[href=?]", users_path
    assert_select "a[href=?]", user_path(@user)
    assert_select "a[href=?]", edit_user_path(@user)
